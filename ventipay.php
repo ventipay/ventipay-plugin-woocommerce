72f612e6c93816305d18dd85fde9bc9840208795
Cristián Meléndez <cmelendez@users.noreply.github.com>
2022-11-10 17:41:58 -0300

    v2.0.2

--- ventipay.php
+++ ventipay.php
@@ -5,7 +5,7 @@
  * Description: Cobra como quieras
  * Author: VentiPay
  * Author URI: https://www.ventipay.com/
- * Version: 2.0.1
+ * Version: 2.0.2
  * Requires at least: 5.7
  * Tested up to: 6.1
  * WC requires at least: 5.0
