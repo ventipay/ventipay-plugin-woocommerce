556a0b5a34bf23f7e98f64cf3caf3deb4858f24c
iTomasi <tduclos@ventipay.com>
2026-07-28 17:13:03 -0400

    v2.4.0

--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: VentiPay
 Tags: ventipay, transbank, webpay, bnpl, chile
 Requires at least: 6.5
-Tested up to: 6.5.3
-Stable tag: 2.3.5
-Requires PHP: 7.0
+Tested up to: 7.0.2
+Stable tag: 2.4.0
+Requires PHP: 7.4
 License: MIT
 License URI: https://github.com/ventipay/ventipay-plugin-woocommerce/blob/main/LICENSE
 
--- ventipay.php
+++ ventipay.php
@@ -5,11 +5,11 @@
  * Description: Cobra como quieras
  * Author: VentiPay
  * Author URI: https://www.ventipay.com/
- * Version: 2.3.5
+ * Version: 2.4.0
  * Requires at least: 6.5
- * Tested up to: 6.5.3
+ * Tested up to: 7.0.2
  * WC requires at least: 8.0.0
- * WC tested up to: 8.9.1
+ * WC tested up to: 10.9.4
  * Text Domain: ventipay
  * Domain Path: /languages
  */
